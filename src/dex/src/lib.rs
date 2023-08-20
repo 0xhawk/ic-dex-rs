5afad52666ca1a1807d25346b816a09826c4facd
0xhawk <hawk@horizonx.tech>
2023-08-20 10:49:53 +0400

    dex skelton

--- src/dex/src/lib.rs
+++ src/dex/src/lib.rs
@@ -1,14 +1,27 @@
 use candid::{candid_method, CandidType, Nat, Principal};
 use ic_cdk::caller;
 use ic_cdk_macros::*;
-use ic_ledger_types::MAINNET_LEDGER_CANISTER_ID;
+use ic_ledger_types::{
+    AccountBalanceArgs, AccountIdentifier, Memo, Tokens, DEFAULT_SUBACCOUNT,
+    MAINNET_LEDGER_CANISTER_ID,
+};
 use std::cell::RefCell;
 
-mod icrc2;
+mod dip20;
+mod exchange;
+mod stable;
+mod types;
+mod utils;
 
-use icrc2::{new_transfer_from, ICRC2};
+use dip20::DIP20;
+use exchange::Exchange;
+use types::*;
+use utils::principal_to_subaccount;
 
 pub type DepositReceipt = Result<Nat, DepositErr>;
+pub type WithdrawReceipt = Result<Nat, WithdrawErr>;
+
+const ICP_FEE: u64 = 10_000;
 
 thread_local! {
     static STATE: RefCell<State> = RefCell::new(State::default());
@@ -18,6 +31,7 @@ thread_local! {
 pub struct State {
     owner: Option<Principal>,
     ledger: Option<Principal>,
+    exchange: Exchange,
 }
 
 #[derive(CandidType)]
@@ -26,46 +40,133 @@ pub enum DepositErr {
     TransferFailure,
 }
 
+#[derive(CandidType)]
+pub enum WithdrawErr {
+    BalanceLow,
+    TransferFailure,
+}
+
 #[update]
 #[candid_method(update)]
-pub async fn deposit(token_canister_id: Principal) -> DepositReceipt {
+pub async fn deposit() -> DepositReceipt {
+    let caller = caller();
+    let ledger_canister_id = STATE
+        .with(|s| s.borrow().ledger)
+        .unwrap_or(MAINNET_LEDGER_CANISTER_ID);
+    let amount = deposit_icp(caller).await?;
+    STATE.with(|s| {
+        s.borrow_mut().exchange.balances.add_balance(
+            &caller,
+            &ledger_canister_id,
+            amount.to_owned(),
+        )
+    });
+    DepositReceipt::Ok(amount)
+}
+
+#[update]
+#[candid_method(update)]
+pub async fn withdraw(amount: Nat, address: Principal) -> WithdrawReceipt {
+    let caller = caller();
+
+    STATE.with(|s| {
+        s.borrow_mut()
+            .exchange
+            .orders
+            .retain(|_, v| v.owner != caller);
+    });
+    let account_id = AccountIdentifier::new(&address, &DEFAULT_SUBACCOUNT);
+    withdraw_icp(&amount, account_id).await
+}
+
+async fn deposit_icp(caller: Principal) -> Result<Nat, DepositErr> {
+    let canister_id = ic_cdk::api::id();
+    let ledger_canister_id = STATE
+        .with(|s| s.borrow().ledger)
+        .unwrap_or(MAINNET_LEDGER_CANISTER_ID);
+
+    let account = AccountIdentifier::new(&canister_id, &principal_to_subaccount(&caller));
+
+    let balance_args = ic_ledger_types::AccountBalanceArgs { account };
+    let balance = ic_ledger_types::account_balance(ledger_canister_id, balance_args)
+        .await
+        .map_err(|_| DepositErr::TransferFailure)?;
+
+    if balance.e8s() < ICP_FEE {
+        return Err(DepositErr::BalanceLow);
+    }
+
+    let transfer_args = ic_ledger_types::TransferArgs {
+        memo: Memo(0),
+        amount: balance - Tokens::from_e8s(ICP_FEE),
+        fee: Tokens::from_e8s(ICP_FEE),
+        from_subaccount: Some(principal_to_subaccount(&caller)),
+        to: AccountIdentifier::new(&canister_id, &DEFAULT_SUBACCOUNT),
+        created_at_time: None,
+    };
+    ic_ledger_types::transfer(ledger_canister_id, transfer_args)
+        .await
+        .map_err(|_| DepositErr::TransferFailure)?
+        .map_err(|_| DepositErr::TransferFailure)?;
+
+    ic_cdk::println!(
+        "Deposit of {} ICP in account {:?}",
+        balance - Tokens::from_e8s(ICP_FEE),
+        &account
+    );
+
+    Ok((balance.e8s() - ICP_FEE).into())
+}
+
+async fn withdraw_icp(amount: &Nat, account_id: AccountIdentifier) -> Result<Nat, WithdrawErr> {
     let caller = caller();
     let ledger_canister_id = STATE
         .with(|s| s.borrow().ledger)
         .unwrap_or(MAINNET_LEDGER_CANISTER_ID);
 
-    let amount = if token_canister_id == ledger_canister_id {
-        deposit_icp(caller).await?
-    } else {
-        deposit_icrc2(caller, token_canister_id).await?
-    };
-    // TODO
-    DepositReceipt::Ok(amount)
-}
-
-async fn deposit_icrc2(caller: Principal, token: Principal) -> Result<Nat, DepositErr> {
-    let token = ICRC2::new(token);
-
-    let available = Nat::from(0);
-
-    let transfer_from_args = new_transfer_from(
-        caller,
-        ic_cdk::api::id(),
-        Nat::from(0),
-        Option::None,
-        Option::None,
-        Option::None,
+    let sufficient_balance = STATE.with(|s| {
+        s.borrow_mut().exchange.balances.subtract_balance(
+            &caller,
+            &ledger_canister_id,
+            amount.to_owned() + ICP_FEE,
+        )
+    });
+    if !sufficient_balance {
+        return Err(WithdrawErr::BalanceLow);
+    }
+    let transfer_amount = Tokens::from_e8s(
+        (amount.to_owned() + ICP_FEE)
+            .0
+            .try_into()
+            .map_err(|_| WithdrawErr::TransferFailure)?,
     );
-
-    token
-        .icrc2_transfer_from(transfer_from_args)
+    let transfer_args = ic_ledger_types::TransferArgs {
+        memo: Memo(0),
+        amount: transfer_amount,
+        fee: Tokens::from_e8s(ICP_FEE),
+        from_subaccount: Some(DEFAULT_SUBACCOUNT),
+        to: account_id,
+        created_at_time: None,
+    };
+    let icp_receipt = ic_ledger_types::transfer(ledger_canister_id, transfer_args)
         .await
-        .map_err(|_| DepositErr::TransferFailure)?;
-    Ok(available)
-}
+        .map_err(|_| WithdrawErr::TransferFailure)
+        .and_then(|v| v.map_err(|_| WithdrawErr::TransferFailure));
 
-async fn deposit_icp(caller: Principal) -> Result<Nat, DepositErr> {
-    Ok(Nat::from(0))
+    if let Err(e) = icp_receipt {
+        STATE.with(|s| {
+            s.borrow_mut().exchange.balances.add_balance(
+                &caller,
+                &ledger_canister_id,
+                amount.to_owned() + ICP_FEE,
+            )
+        });
+        return Err(e);
+    }
+
+    ic_cdk::println!("Withdrawal of {} ICP to account {:?}", amount, &account_id);
+
+    Ok(amount.to_owned() + ICP_FEE)
 }
 
 #[init]
